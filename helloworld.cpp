15279d6a661f4eb75406ec528932ebb46b80110d
Rezi <rezi.sirbiladze2@gmail.com>
2023-05-24 15:15:13 +0200

    ADD EX 9

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,11 @@
 // https://www.youtube.com/watch?v=-TkoO8Z07hI&ab_channel=BroCode
+// https://cplusplus.com/reference/
 
 #include <iostream>
+
+// 9 Useful math related functions
+#include <cmath>
+//* 9 Useful math related functions
 
 // 5 Typedef and type aliases
 #include <vector>
@@ -25,19 +30,51 @@ namespace second
 
 int main()
 {
-    // 6 Arithmetic operators
+    // 9 Useful math related functions
     std::cout << std::endl
-              << 6 << std::endl;
-    int students = 20;
-    students = students + 1;
-    students += 1;
-    students /= 2;
-    students++;
-    int remainder = students % 2;
-    std::cout << students;
+              << 9 << std::endl;
+    double x9 = 3.14;
+    double y9 = 4.59;
+    double z9;
+    z9 = std::max(x9, y9);
+    z9 = std::min(x9, y9);
+    z9 = pow(x9, y9);
+    z9 = sqrt(x9);
+    z9 = abs(x9);
+    z9 = ceil(x9);
+    z9 = round(x9);
+    z9 = floor(x9);
+    std::cout << z9;
     std::cout << std::endl
-              << 6 << std::endl;
-    //* 6 Arithmetic operators
+              << 9 << std::endl;
+    //* 9 Useful math related functions
+
+    // 8 User input
+    std::cout << std::endl
+              << 8 << std::endl;
+    int age8;
+    std::string name8;
+    std::cout << "What's your name?: ";
+    // "std::ws" to clear buffer
+    std::getline(std::cin >> std::ws, name8);
+    std::cout << "What's your age?: ";
+    std::cin >> age8;
+    std::cout << "Hello " << name8 << '\n';
+    std::cout << "you are " << age8 << " years old";
+    std::cout << std::endl
+              << 8 << std::endl;
+    //* 8 User input
+
+    // 7 Type conversion
+    std::cout << std::endl
+              << 7 << std::endl;
+    int correct = 8;
+    int questions = 10;
+    double score = correct / (double)questions * 100;
+    std::cout << score << '%';
+    std::cout << std::endl
+              << 7 << std::endl;
+    //* 7 Type conversion
 
     // 6 Arithmetic operators
     std::cout << std::endl
@@ -90,7 +127,7 @@ int main()
     int x = 5;
     int y = 4;
     int sum = 5 + 4;
-    std::cout << "suma de " << x << " y " << y << " = " << sum << '\n';
+    std::cout << "Sum of " << x << " and " << y << " = " << sum << '\n';
 
     double temperature = 25.1;
     char grade = 'A';
