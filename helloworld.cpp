a87ce764742c3f6d7babce1642390b6ab0343199
Rezi <rezi.sirbiladze2@gmail.com>
2023-06-08 20:52:56 +0200

    ADD EX 43

--- helloworld.cpp
+++ helloworld.cpp
@@ -47,6 +47,8 @@ void sortAnArray_38();
 void fillFunction_39();
 void fillAnArrayWithUserInput_40();
 void multidimensionalArrays_41();
+void quizGame_42();
+void memoryAddresses_43();
 //* Functions
 
 //
@@ -92,7 +94,7 @@ int main()
     std::string name = "Rezi";
     int age = 23;
 
-    multidimensionalArrays_41();
+    memoryAddresses_43();
 
     // std::cout << "Result: " << returnKeyword_27() << "";
 
@@ -1080,4 +1082,93 @@ void multidimensionalArrays_41()
             std::cout << cars[i][j] << '\n';
         }
     }
+}
+
+void quizGame_42()
+{
+    std::cout << '\n'
+              << "************** Quiz Game **************" << '\n';
+
+    std::string questions[] = {"1. What year was C++ created?: ",
+                               "2. Who invented C++?: ",
+                               "3. What is the predecessor of C++?: ",
+                               "4. Is the Earth flat?"};
+
+    std::string options[][4] = {
+        {
+            "A. 1969",
+            "B. 1975",
+            "C. 1985",
+            "D. 1989",
+        },
+        {
+            "A. Guido van Rossum",
+            "B. Bjarne Stroustrup",
+            "C. Honh Carmack",
+            "D. Mark Zuckerburg",
+        },
+        {
+            "A. C",
+            "B. C+",
+            "C. C--",
+            "D. B++",
+        },
+        {
+            "A. Yes",
+            "B. No",
+            "C. Sometimes",
+            "D. What's Earth?",
+        }};
+
+    char answerKey[] = {'C', 'B', 'A', 'B'};
+
+    int size = sizeof(questions) / sizeof(questions[0]);
+    char guess;
+    int score = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+
+        std::cout << '\n'
+                  << "*******************" << '\n';
+        std::cout << questions[i] << '\n';
+        std::cout << "*******************" << '\n';
+        for (int j = 0; j < sizeof(options[i]) / sizeof(options[i][0]); j++)
+        {
+            std::cout << options[i][j] << '\n';
+        }
+        std::cin >> guess;
+        guess = toupper(guess);
+        if (guess == answerKey[i])
+        {
+            std::cout << "Correct" << '\n';
+            score++;
+        }
+        else
+        {
+            std::cout << "Wrong" << '\n';
+            std::cout << "Answer is: " << answerKey[i] << '\n';
+        }
+        std::cout << "*******************" << '\n';
+    }
+
+    std::cout << '\n'
+              << "*******************" << '\n';
+    std::cout << "Correct guesses:" << score << '\n';
+    std::cout << "# of questions:" << size << '\n';
+    std::cout << "Score: " << (score / (double)size) * 100 << '%' << '\n';
+    std::cout << "*******************" << '\n';
+
+    std::cout << "***************************************" << '\n';
+}
+
+void memoryAddresses_43()
+{
+    std::string name = "Rezi";
+    int age = 23;
+    bool student = true;
+
+    std::cout << &name << '\n';
+    std::cout << &age << '\n';
+    std::cout << &student << '\n';
 }
